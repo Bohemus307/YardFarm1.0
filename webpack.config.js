01a85db02fc5db493908cd9a794d7a15b88228a7
joshua oxner <joshuaoxner@joshuas-MacBook-Pro.local>
2020-06-06 16:55:48 -0600

    Got it running commit for jeebus sakes

--- webpack.config.js
+++ webpack.config.js
@@ -6,25 +6,15 @@ module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
-    path: DIST_DIR
+    path: DIST_DIR,
   },
-  module : {
+  module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader", "eslint-loader"]
-      }
+        test: /\.jsx?$/,
+        include: [SRC_DIR],
+        use: 'babel-loader',
+      },
     ],
-    loaders : [
-      {
-        test : /\.jsx?/,
-        include : SRC_DIR,
-        loader : 'babel-loader',      
-        query: {
-          presets: ['react', 'es2015']
-       }
-      }
-    ]
-  }
+  },
 };
