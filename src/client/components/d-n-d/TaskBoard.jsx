486fb4c46c0de33fb09d10a0477f3685a3550735
joshua oxner <bohemus@hotmail.com>
2020-07-14 08:14:52 -0600

    drag and drop functioning now time for some css

--- src/client/components/d-n-d/TaskBoard.jsx
+++ src/client/components/d-n-d/TaskBoard.jsx
@@ -14,8 +14,33 @@ class TaskBoard extends React.Component {
   constructor(props) {
     super(props)
 
-    this.state = { initialData };
-  }
+    this.state = {
+      tasks: {
+        'task-1': { id: 'task-1', content: 'take out the trash'},
+        'task-2': { id: 'task-2', content: 'task2'},
+        'task-3': { id: 'task-3', content: 'task3'},
+        'task-4': { id: 'task-4', content: 'task4'},
+      },
+      columns: {
+        'column-1': {
+          id: 'column-1',
+          title: 'To do',
+          taskIds: ['task-1', 'task-2', 'task-3', 'task-4'],
+        },
+        'column-2': {
+          id: 'column-2',
+          title: 'In Progress',
+          taskIds: [],
+        },
+        'column-3': {
+          id: 'column-3',
+          title: 'Done',
+          taskIds: [],
+        },
+      },
+      columnOrder: ['column-1', 'column-2', 'column-3'],
+    }
+  };
   
   onDragEnd = result => {
     const { destination, source, draggableId } = result;
@@ -30,8 +55,8 @@ class TaskBoard extends React.Component {
       return;
     }
 
-    const start = this.state.initialData.columns[source.droppableId];
-    const finish = this.state.initialData.columns[destination.droppableId];
+    const start = this.state.columns[source.droppableId];
+    const finish = this.state.columns[destination.droppableId];
 
     if (start === finish) {
       const newTaskIds = Array.from(start.taskIds);
@@ -44,7 +69,7 @@ class TaskBoard extends React.Component {
       };
 
       const newState = {
-        ...this.state.initialData,
+        ...this.state,
         columns: {
           ...this.state.columns,
           [newColumn.id]: newColumn,
@@ -71,7 +96,7 @@ class TaskBoard extends React.Component {
     };
 
     const newState = {
-      ...this.state.initialData,
+      ...this.state,
       columns: {
         ...this.state.columns,
         [newStart.id]: newStart,
@@ -86,9 +111,9 @@ class TaskBoard extends React.Component {
     return(
       <DragDropContext onDragEnd={this.onDragEnd}>
         <Container>
-        {this.state.initialData.columnOrder.map(columnId => {
-          const column = this.state.initialData.columns[columnId];
-          const tasks = column.taskIds.map(taskId => this.state.initialData.tasks[taskId],);
+        {this.state.columnOrder.map(columnId => {
+          const column = this.state.columns[columnId];
+          const tasks = column.taskIds.map(taskId => this.state.tasks[taskId],);
       
           return <Column key= {column.id} column={column} tasks={tasks} />;
           })}
