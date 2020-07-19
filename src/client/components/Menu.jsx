d1cf9703990bd593f1562bcbdb4422e45ec95f07
joshua oxner <bohemus@hotmail.com>
2020-07-18 18:49:26 -0600

    switches are hooks based but need dynamic ids

--- src/client/components/Menu.jsx
+++ src/client/components/Menu.jsx
@@ -1,67 +1,74 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import React, { useState } from 'react';
-import Switch from "./Switch.jsx";
+import Switch from './Switch.jsx';
 
-const Menu = () => (
-  <div className="Menu">
-    <div className="icon">
-      <img className="logo-1" src="/images/greenhouse3.png" alt="company logo here" />
-    </div>
-    <div className="logo_text">
-      Yard Farm
-    </div>
-    <div className="search-container">
-      <form action="/action_page.php">
-        <input type="text" placeholder="mm/dd/yy.." name="search" />
-        <button type="submit"><i className="fa fa-search" /></button>
-      </form>
-    </div>
-    <hr />
-    <div className="switch_panel">
-      <div className="control_panel">
-        <span className="switch_title">Farm Controls</span>
-        <Switch />
-        <div className="switch_div">
-          <label className="switch">
-            <input type="checkbox" />
-            <span className="slider round" />
-          </label>
-          <img className="heat_icon small_icon" src="/images/heater.png" alt="heat" title="Heat" />
-        </div>
-        <div className="switch_div">
-          <label className="switch">
-            <input type="checkbox" />
-            <span className="slider round" />
-          </label>
-          <img className="camera_icon small_icon" src="/images/photo.png" alt="Camera" title="Camera" />
-        </div>
-        <div className="switch_div">
-          <label className="switch">
-            <input type="checkbox" />
-            <span className="slider round" />
-          </label>
-          <img className="notify_icon small_icon" src="/images/contact.png" alt="Notify" title="Notify Me" />
-        </div>
+const Menu = () => {
+  const [value, setValue] = useState(false);
+  return (
+    <div className="Menu">
+      <div className="icon">
+        <img className="logo-1" src="/images/greenhouse3.png" alt="company logo here" />
+      </div>
+      <div className="logo_text">
+        Yard Farm
+      </div>
+      <div className="search-container">
+        <form action="/action_page.php">
+          <input type="text" placeholder="mm/dd/yy.." name="search" />
+          <button type="submit"><i className="fa fa-search" /></button>
+        </form>
       </div>
       <hr />
-      <div className="nutrients_table">
-        <span className="nutrients_header">Current Nutrients</span>
-        <div id="progressbar">
-          <div className="progressbar_div1" />
+      <div className="switch_panel">
+        <div className="control_panel">
+          <span className="switch_title">Farm Controls</span>
+          <Switch
+            id="switch-1"
+            isOn={value}
+            handleToggle={() => setValue(!value)}
+            image="/images/fan.png"
+          />
+          <Switch
+            id="switch-2"
+            isOn={value}
+            handleToggle={() => setValue(!value)}
+            image="/images/heater.png"
+          />
+          <div className="switch_div">
+            <label className="switch">
+              <input type="checkbox" />
+              <span className="slider round" />
+            </label>
+            <img className="camera_icon small_icon" src="/images/photo.png" alt="Camera" title="Camera" />
+          </div>
+          <div className="switch_div">
+            <label className="switch">
+              <input type="checkbox" />
+              <span className="slider round" />
+            </label>
+            <img className="notify_icon small_icon" src="/images/contact.png" alt="Notify" title="Notify Me" />
+          </div>
         </div>
-        <span className="nutrient_label">PH</span>
-        <div id="progressbar">
-          <div className="progressbar_div2" />
+        <hr />
+        <div className="nutrients_table">
+          <span className="nutrients_header">Current Nutrients</span>
+          <div id="progressbar">
+            <div className="progressbar_div1" />
+          </div>
+          <span className="nutrient_label">PH</span>
+          <div id="progressbar">
+            <div className="progressbar_div2" />
+          </div>
+          <span className="nutrient_label">EC</span>
+          <div id="progressbar">
+            <div className="progressbar_div3" />
+          </div>
+          <span className="nutrient_label">PPM</span>
         </div>
-        <span className="nutrient_label">EC</span>
-        <div id="progressbar">
-          <div className="progressbar_div3" />
-        </div>
-        <span className="nutrient_label">PPM</span>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Menu;
