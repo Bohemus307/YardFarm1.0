71dfbf5a271812c74c93a672a6034b78b12a7f86
joshua oxner <bohemus@hotmail.com>
2020-08-21 06:50:34 -0400

    adding trello style component

--- src/client/components/Second/Second.jsx
+++ src/client/components/Second/Second.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import classes from './Second.css';
-
+import TaskBoard from '../d-n-d/TaskBoard.jsx';
 // import TaskBoard from '../d-n-d/TaskBoard.jsx';
 
 class Second extends React.Component {
@@ -44,13 +44,15 @@ class Second extends React.Component {
   render() {
     return (
       <div className={classes.Second_div}>
-        <div>
+        {/* <div>
           <form className={classes.Note_pad} onSubmit={this.handleSubmit}>
             <textarea placeholder="Notes..." className={classes.Input_area} type="textarea" value={this.state.value} onChange={this.handleChange} />
             <input className={classes.Notes_button} type="submit" value="Save" />
           </form>
+        </div> */}
+        <div className={classes.Drag_N_Drop}>
+          <TaskBoard />
         </div>
-
       </div>
     );
   }
