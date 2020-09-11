7cd72020c97866ef658d3f3925d73ca0025a6690
joshua oxner <bohemus@hotmail.com>
2020-09-11 07:09:25 -0600

    closer but it fo now

--- src/client/components/NutrientsTable/NutrientsTable.jsx
+++ src/client/components/NutrientsTable/NutrientsTable.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import classes from './NutrientsTable.css';
 
 const NutrientsTable = () => (
-  <div className={classes.Nutrients_table}>
-    <div id="progressbar" className={classes.ProgressBar}>
-      <div className={classes.Progressbar_div1} />
+  <div className={classes.Table_wrappper}>
+    <div className={classes.Nutrients_header}>
+      <span>Current Nutrient Readings</span>
     </div>
-    <div id="progressbar" className={classes.ProgressBar}>
-      <div className={classes.Progressbar_div2} />
-    </div>
-    <div id="progressbar" className={classes.ProgressBar}>
-      <div className={classes.Progressbar_div3} />
+    <div className={classes.Nutrients_table}>
+      <div id="progressbar" className={classes.ProgressBar}>
+        <div className={classes.Progressbar_div1} />
+      </div>
+      <div id="progressbar" className={classes.ProgressBar}>
+        <div className={classes.Progressbar_div2} />
+      </div>
+      <div id="progressbar" className={classes.ProgressBar}>
+        <div className={classes.Progressbar_div3} />
+      </div>
     </div>
   </div>
 );
