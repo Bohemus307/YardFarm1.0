5633be6141eb3cd24a791e6e417876b1bda72d8f
joshua oxner <bohemus@hotmail.com>
2020-09-12 09:04:57 -0600

    got nutriemts table where i want it

--- src/client/components/NutrientsTable/NutrientsTable.jsx
+++ src/client/components/NutrientsTable/NutrientsTable.jsx
@@ -7,15 +7,41 @@ const NutrientsTable = () => (
       <span>Current Nutrient Levels</span>
     </div>
     <div className={classes.Nutrients_table}>
-      <span className={classes.Feed_Label}>PH</span>
-      <div id="progressbar" className={classes.Progress_Bar}>
-        <div className={classes.Progressbar_div1} />
+      <div className={classes.Table_item}>
+        <span className={classes.Feed_label}>PH</span>
+        <div id="progressbar" className={classes.Progress_Bar}>
+          <div className={classes.Progressbar_div1} />
+        </div>
       </div>
-      <div id="progressbar" className={classes.Progress_Bar}>
-        <div className={classes.Progressbar_div2} />
+      <div className={classes.Table_item}>
+        <span className={classes.Feed_label}>EC</span>
+        <div id="progressbar" className={classes.Progress_Bar}>
+          <div className={classes.Progressbar_div2} />
+        </div>
       </div>
-      <div id="progressbar" className={classes.Progress_Bar}>
-        <div className={classes.Progressbar_div3} />
+      <div className={classes.Table_item}>
+        <span className={classes.Feed_label}>Water Temp</span>
+        <div id="progressbar" className={classes.Progress_Bar}>
+          <div className={classes.Progressbar_div1} />
+        </div>
+      </div>
+      <div className={classes.Table_item}>
+        <span className={classes.Feed_label}>PPM</span>
+        <div id="progressbar" className={classes.Progress_Bar}>
+          <div className={classes.Progressbar_div3} />
+        </div>
+      </div>
+      <div className={classes.Table_item}>
+        <span className={classes.Feed_label}>ORP</span>
+        <div id="progressbar" className={classes.Progress_Bar}>
+          <div className={classes.Progressbar_div2} />
+        </div>
+      </div>
+      <div className={classes.Table_item}>
+        <span className={classes.Feed_label}>Flow Rate</span>
+        <div id="progressbar" className={classes.Progress_Bar}>
+          <div className={classes.Progressbar_div3} />
+        </div>
       </div>
     </div>
   </div>
