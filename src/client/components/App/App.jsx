ce1b8a337ba37d45cf7c4da043beed73a7440a9f
joshua oxner <bohemus@hotmail.com>
2020-10-06 09:34:13 -0600

    added login form base

--- src/client/components/App/App.jsx
+++ src/client/components/App/App.jsx
@@ -7,52 +7,51 @@ import {
 } from 'react-router-dom';
 
 import Dashboard from '../../Containers/DashBoard/Dashboard.jsx';
+
 import Login from '../Login/Login.jsx';
 
-export default function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Login</Link>
-            </li>
-            <li>
-              <Link to="/dashboard">Dash Board</Link>
-            </li>
-            <li>
-              <Link to="/alerts">Alerts</Link>
-            </li>
-            <li>
-              <Link to="/controls">Controls</Link>
-            </li>
-          </ul>
-        </nav>
+const App = () => (
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Login</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dash Board</Link>
+          </li>
+          <li>
+            <Link to="/alerts">Alerts</Link>
+          </li>
+          <li>
+            <Link to="/controls">Controls</Link>
+          </li>
+        </ul>
+      </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
+      {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/alerts">
-            <Alerts />
-          </Route>
-          <Route path="/controls">
-            <Controls />
-          </Route>
-          <Route path="/admin">
-            <Admin />
-          </Route>
-          <Route path="/">
-            <Login />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
-}
+      <Switch>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+        <Route path="/alerts">
+          <Alerts />
+        </Route>
+        <Route path="/controls">
+          <Controls />
+        </Route>
+        <Route path="/admin">
+          <Admin />
+        </Route>
+        <Route path="/">
+          <Login />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+);
 
 function Alerts() {
   return <h2>Alerts</h2>;
@@ -65,3 +64,5 @@ function Controls() {
 function Admin() {
   return <h2>Admin</h2>;
 }
+
+export default App;
--- src/client/components/Login/Login.jsx
+++ src/client/components/Login/Login.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 
-export default function Login() {
-  return (
+import classes from './Login.css';
+import Logo from '../Logo/Logo.jsx';
+
+const Login = () => (
+  <div className={classes.Login}>
+    <Logo height="50%" />
+    <span>Yard Farm</span>
     <div>
-      hello from login
+      <form className={classes.Login_Form}>
+        <label htmlFor="username">User Name:</label>
+        <br />
+        <input type="text" id="username" name="username" />
+        <label htmlFor="password">Password:</label>
+        <br />
+        <input type="text" id="password" name="password" />
+        <input type="submit" value="Submit"></input>
+      </form>
     </div>
-  );
-}
+
+  </div>
+);
+
+export default Login;
