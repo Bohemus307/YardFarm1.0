3a4f236e9999789711cb105a8e89ab4ec3c13193
joshua oxner <bohemus@hotmail.com>
2020-10-06 12:01:14 -0600

    major css for login page

--- src/client/components/Login/Login.jsx
+++ src/client/components/Login/Login.jsx
@@ -5,20 +5,27 @@ import Logo from '../Logo/Logo.jsx';
 
 const Login = () => (
   <div className={classes.Login}>
-    <Logo height="50%" />
-    <span className={classes.Title}>Yard Farm</span>
-    <div>
-      <form className={classes.Login_Form}>
-        <label className={classes.FormLabel} htmlFor="username">User Name:</label>
-        <br />
-        <input className={classes.FormInput} type="text" id="username" name="username" />
-        <label className={classes.FormLabel} htmlFor="password">Password:</label>
-        <br />
-        <input className={classes.FormInput} type="text" id="password" name="password" />
-        <input className={classes.Submit} type="submit" value="Submit"></input>
-      </form>
+    <div className={classes.Container}>
+      <div className={classes.LeftDiv}>
+        <Logo height="80%" />
+      </div>
+      <div className={classes.RightDiv}>
+        <div className={classes.Title}>Yard Farm Login</div>
+        <form className={classes.Login_Form}>
+          <br />
+          <input className={classes.FormInput} type="text" id="username" name="username" placeholder="Username" />
+          <span>
+            <img className={classes.InputImage} src="/images/id-card.png" alt="username" title="username" />
+          </span>
+          <br />
+          <input className={classes.FormInput} type="password" id="password" name="password" placeholder="Password" />
+          <span>
+            <img className={classes.InputImage} src="/images/password.png" alt="password" title="password" />
+          </span>
+          <input className={classes.Submit} type="submit" value="Submit" id="submit" />
+        </form>
+      </div>
     </div>
-
   </div>
 );
 
