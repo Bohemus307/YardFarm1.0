6952866ebf31536af4ce3f5316709ecba5a93213
joshua oxner <bohemus@hotmail.com>
2020-10-06 11:16:56 -0600

    for login page base

--- src/client/components/Login/Login.jsx
+++ src/client/components/Login/Login.jsx
@@ -6,16 +6,16 @@ import Logo from '../Logo/Logo.jsx';
 const Login = () => (
   <div className={classes.Login}>
     <Logo height="50%" />
-    <span>Yard Farm</span>
+    <span className={classes.Title}>Yard Farm</span>
     <div>
       <form className={classes.Login_Form}>
-        <label htmlFor="username">User Name:</label>
+        <label className={classes.FormLabel} htmlFor="username">User Name:</label>
         <br />
-        <input type="text" id="username" name="username" />
-        <label htmlFor="password">Password:</label>
+        <input className={classes.FormInput} type="text" id="username" name="username" />
+        <label className={classes.FormLabel} htmlFor="password">Password:</label>
         <br />
-        <input type="text" id="password" name="password" />
-        <input type="submit" value="Submit"></input>
+        <input className={classes.FormInput} type="text" id="password" name="password" />
+        <input className={classes.Submit} type="submit" value="Submit"></input>
       </form>
     </div>
 
