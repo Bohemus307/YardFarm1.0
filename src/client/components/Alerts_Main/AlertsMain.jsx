1374576bb0a2fce25aaee5772f9ad440a53e38bd
joshua oxner <bohemus@hotmail.com>
2020-12-23 06:17:35 -0800

    feature(alerts main): began construction of alerts main section

--- src/client/components/Alerts_Main/AlertsMain.jsx
+++ src/client/components/Alerts_Main/AlertsMain.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import propTypes from 'prop-types';
 
-const  AlertsMain = () => {
-return (
-);
+const AlertsMain = () => {
+  const [alerts, setAlerts] = useState({
+    currentAlerts: {
+      Ph: {
+        max: 8,
+        min: 4,
+        currentValue: 0,
+      },
+    },
+  });
+  return (
+    <div>hello</div>
+  );
 };
 
 AlertsMain.propTypes = {
-name: propTypes.string.isRequired
+// name: propTypes.string.isRequired,
 };
 
 export default AlertsMain;
