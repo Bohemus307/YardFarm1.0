f641895d64600bc124c998da91756303e866bd9c
joshua oxner <bohemus@hotmail.com>
2021-01-23 09:25:06 -0800

    feat(alerts chart): chart component complete now to add drop down implementation

--- src/client/components/AlertChart/AlertChart.jsx
+++ src/client/components/AlertChart/AlertChart.jsx
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import c3 from 'c3';
 import propTypes from 'prop-types';
 
 const AlertChart = () => {
-  const alertsChart = c3.generate({
-    bindto: '#alertsChart',
-    data: {
-      columns: [
-        ['data1', 300, 350, 300, 0, 0, 120],
-        ['data2', 130, 100, 140, 200, 150, 50],
-      ],
-      types: {
-        data1: 'area-spline',
-        data2: 'area-spline',
-        // 'line', 'spline', 'step', 'area', 'area-step' are also available to stack
-      },
-      groups: [['data1', 'data2']],
+  const [data, setData] = useState({
+    columns: [
+      ['In Alarm', 300, 350, 300, 0, 0, 120],
+      ['Out Of Alarm', 130, 100, 140, 200, 150, 50],
+    ],
+    types: {
+      'In Alarm': 'area-spline',
+      'Out Of Alarm': 'area-spline',
+      // 'line', 'spline', 'step', 'area', 'area-step' are also available to stack
     },
+    groups: [['In Alarm', 'Out Of Alarm']],
+  });
+
+  const updateChart = () => {
+    const alertsChart = c3.generate({
+      bindto: '#alertsChart',
+      data,
+    });
+  };
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Update the chart
+    updateChart();
   });
 
   return (
-    <div style={{ height: '200px' }} id="alertsChart" />
+    <div>
+      <div style={{ height: '400px' }} id="alertsChart" />
+    </div>
   );
 };
 
