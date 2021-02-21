c891e36820508fc784535a8322f70331c5c4f36c
joshua oxner <bohemus@hotmail.com>
2021-02-21 06:44:42 -0700

    feature(generators): creating random number generators for data in generators folder in server.

--- src/client/components/UI/Modal/Modal.jsx
+++ src/client/components/UI/Modal/Modal.jsx
@@ -3,17 +3,17 @@ import Wrapper from '../../../HOC/Wrapper/Wrapper.jsx';
 import Backdrop from '../Backdrop/Backdrop.jsx';
 import classes from './Modal.css';
 
-const Modal = (props) => (
+const Modal = ({ show, modalClosed, children }) => (
   <Wrapper>
-    <Backdrop show={props.show} clicked={props.modalClosed} />
+    <Backdrop show={show} clicked={modalClosed} />
     <div
       style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0',
+        transform: show ? 'translateY(0)' : 'translateY(-100vh)',
+        opacity: show ? '1' : '0',
       }}
       className={classes.Modal}
     >
-      {props.children}
+      {children}
     </div>
   </Wrapper>
 );
---
+++ src/server/generators/generators.js
@@ -0,0 +1,5 @@
+module.exports = {
+  createDataForEnviromentals: (dataType, unitOfMeasure) => {
+    console.log(dataType, unitOfMeasure);
+  },
+};
