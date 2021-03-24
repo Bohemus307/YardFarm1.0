3aed318425af3c7a0471a90be1b1435acd16fea5
joshua oxner <bohemus@hotmail.com>
2021-03-24 17:55:44 -0600

    feature(alert sliders): config passed successfully to sliders

--- src/client/components/AlertSettings/AlertSettings.jsx
+++ src/client/components/AlertSettings/AlertSettings.jsx
@@ -6,6 +6,8 @@ import React, {
 
 import propTypes from 'prop-types';
 // import classes from './Alerts.css';
+import AlertSliders from '../AlertSlider/AlertSlider.jsx';
+import AlertSlider from '../AlertSlider/AlertSlider.jsx';
 
 const AlertsSettings = ({ alerts }) => {
   // parse alert obj to array of alert groups
@@ -28,12 +30,20 @@ const AlertsSettings = ({ alerts }) => {
       </div>
       <h3>{alert.id}</h3>
       <div>
-        slider
+        <AlertSlider config={Object.keys(alert.config).reduce((arr, key, idx) => {
+          const object = {
+            id: key,
+            config: Object.values(alert.config)[idx],
+          };
+          arr.push(object);
+          return arr;
+        }, [])}
+        />
       </div>
     </div>
   ));
 
-  console.log(alertsGroupArray);
+  //console.log(alertsGroupArray);
   return (
     <div>
       <ul>
--- src/client/components/AlertSlider/AlertSlider.jsx
+++ src/client/components/AlertSlider/AlertSlider.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import propTypes from 'prop-types';
 
-const AlertSlider = () => {
-return (
-);
+const AlertSlider = ({ config }) => {
+  
+  console.log('config in slider', config);
+
+  return (
+    <div>
+      sliders
+    </div>
+  );
 };
 
- AlertSlider.propTypes = {
-name: propTypes.string.isRequired
+AlertSlider.propTypes = {
 };
 
 export default AlertSlider;
