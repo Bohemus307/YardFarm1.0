4e0086072301a6d08ac14fa784329acc5abbe467
joshua oxner <bohemus@hotmail.com>
2021-03-25 07:09:51 -0600

    feature(sliders for alerts): slider implemented now for css and testing

--- src/client/components/AlertSliders/AlertSliders.jsx
+++ src/client/components/AlertSliders/AlertSliders.jsx
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
 import propTypes from 'prop-types';
 
-// import Alarms from '';
+import classes from './AlertSliders.css';
+import RangeSlider from '../Slider/Slider.jsx';
 
 const AlertSliders = ({ config }) => {
-  // console.log('config in slider', config);
-  const sliderList = config.map((alertConfig) => {
-    // each config needs to be turned to slider
-    
-  });
+  console.log('config in slider', config);
+  const sliderList = config.map((alertConfig) => (
+    <RangeSlider
+      classes={classes.Slider}
+      type={alertConfig.id}
+      key={alertConfig.id}
+      UOM={alertConfig.config.UOM || null}
+      // {...slideProps}
+      // disabled={slider.locked}
+    />
+  ));
   return (
     <div>
       {sliderList}
